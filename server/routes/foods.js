2687aed326944d12db794a6ef1c03fc753c1352b
10tion <attentionlee06@gmail.com>
2018-11-26 04:37:57 -0800

    minor router rule modification

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -6,4 +6,8 @@ router.get('/:foodid', async (ctx, next) => {
   ctx.body = 'you are querying ' + ctx.params.foodid
 })
 
+// /?foodID=xxx
+// /?restaurantID=xxx
+// ?restaurantID=xxx&page=4&pre_page=10
+
 module.exports = router

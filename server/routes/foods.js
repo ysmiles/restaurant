de5d1b1c7db8716e1194933dd4f8e1dfd82128b6
10tion <attentionlee06@gmail.com>
2018-11-30 06:51:32 -0800

    real API? idk

--- server/routes/foods.js
+++ server/routes/foods.js
@@ -1,9 +1,59 @@
 const router = require('koa-router')()
+const Food = require('../models/Food')
 
-router.prefix('/foods')
+// all return obejcts will be converted to JSON
+const json = require('koa-json')
 
-router.get('/:foodid', async (ctx, next) => {
-  ctx.body = 'you are querying ' + ctx.params.foodid
+// disable the prefix setting
+//router.prefix('/foods')
+
+// all get method 
+router.get('/foods', async (ctx, next) => {
+
+  let queryProps = { where: {} }
+
+  if (ctx.query.restaurantId) {
+    queryProps.restaurant_id = ctx.query.restaurantId
+  }
+
+  // search for the keyword, no inside blank allowed
+  if (ctx.query.search) {
+    let keyWords = '%' + ctx.query.search.trim() + '%'
+    console.log('search for all foods match: ' + ctx.query.search)
+
+    // add food name property to query statement
+    queryProps.where.name = { [Op.like]: keyWords }
+
+
+  }
+
+  // The search mode and foodID query is incompatible and search mode has priority over foodID
+  else if (ctx.query.foodId) {
+    queryProps.where.item_id = ctx.query.foodID
+  }
+
+  if (ctx.query.page && ctx.query.per_page) {
+    let pageInt = parseInt(ctx.query.page)
+    let perInt = parseInt(ctx.query.per_page)
+    let offsetInt = (pageInt - 1) * perInt
+
+    queryProps.offset = offsetInt
+    queryProps.limit = perInt
+  }
+
+
+  Food
+    .findAll(queryProps)
+    .then(result => {
+      ctx.body = result
+    })
+    .catch(err => console.log(err))
+
+  return
+})
+
+router.post('/foods', async (cyx, next) => {
+  
 })
 
 // /?foodID=xxx

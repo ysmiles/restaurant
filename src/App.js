05828f6f34f6a5bbb5edeacab1998360c3af2114
ys-pc <ys@ysmiles.com>
2018-12-07 05:05:55 -0800

    add map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { view as Navigation } from './features/navigation';
+import { view as MapModule } from './features/map';
 
 import Router from './Router';
 
@@ -12,6 +13,7 @@ function App() {
       <MuiThemeProvider>
         <Navigation />
         <Router />
+        <MapModule />
       </MuiThemeProvider>
     </div>
   );
--- src/features/map/views/featureName.js
+++ src/features/map/views/featureName.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 class FeatureName extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+  // constructor(props, context) {
+  //   super(props, context);
 
-    this.onSubmit = this.onSubmit.bind(this);
-    this.refInput = this.refInput.bind(this);
-  }
+  //   this.onSubmit = this.onSubmit.bind(this);
+  //   this.refInput = this.refInput.bind(this);
+  // }
 
-  onSubmit(event) {
-    // event.preventDefault();
-    // do something
-  }
+  // onSubmit(event) {
+  //   // event.preventDefault();
+  //   // do something
+  // }
 
-  refInput() {}
+  // refInput() {}
+
+  // render() {
+  //   return (
+  //     <div>
+  //       <h3>This is the title</h3>
+  //       This is the content
+  //     </div>
+  //   );
+  // }
+  static defaultProps = {
+    center: {
+      lat: 37.34,
+      lng: -121.89
+    },
+    zoom: 12
+  };
 
   render() {
     return (
-      <div>
-        <h3>This is the title</h3>
-        This is the content
+      // Important! Always set the container height explicitly
+      <div style={{ height: '50vh', width: '50%' }}>
+        <GoogleMapReact
+          // Put key at correct place
+          bootstrapURLKeys={{ key: '' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.34}
+            lng={-121.89}
+            text={'My customized text'}
+          />
+        </GoogleMapReact>
       </div>
     );
   }

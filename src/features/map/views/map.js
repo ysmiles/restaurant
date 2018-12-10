7a813e81d533f1c66f1d3eb7e59719ac87b02d14
ysmiles <ys@ysmiles.com>
2018-12-10 12:27:20 -0800

    optimize map

--- src/features/map/views/map.js
+++ src/features/map/views/map.js
@@ -78,17 +78,18 @@ class FeatureName extends React.Component {
   drawRoutes(routes, map, maps) {
     let directions = routes;
     let directionsService = new maps.DirectionsService();
-    for (var j = 0; directions[j] != null; j++) {
+
+    directions.forEach(element => {
       let directionsDisplay = new maps.DirectionsRenderer();
       directionsDisplay.setMap(map);
-      directionsService.route(directions[j], function(response, status) {
+      directionsService.route(element, function(response, status) {
         if (status === 'OK') {
           directionsDisplay.setDirections(response);
         } else {
           // window.alert("Directions request failed due to " + status);
         }
       });
-    }
+    });
   }
 
   render() {
